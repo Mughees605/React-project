3e40beb497630d1e1f1b6796ccb91e8595d2eacc
mughees605 <mughees605@gmail.com>
2016-12-29 03:12:48 +0200

    main component complete react boilerplate 1

---
+++ ReactBoilerplate/app/components/main.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+//var ReactDOM = require('react-dom');
+
+var Main = React.createClass({
+    render:function(){
+        return <h1>Main</h1>
+    }
+})
+module.exports = Main;
